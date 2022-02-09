2222bcae48681ab443ec1161624b021607300bcc
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-08 17:43:49 -0800

    orb list rendering

--- src/Orb.js
+++ src/Orb.js
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
 
-
-export default function Orb() {
+export default function Orb({ orb }) {
   return (
-    <div>Orb</div>
+    <Link to={`/orbs/${orb.name}`}>
+      <div className='orb'>
+        <p>{orb.name}</p>
+        <p>{orb.bodyType}</p>
+        <p>{orb.discoveryDate}</p>
+        <p>more info...</p>
+
+      </div>
+    </Link>
+ 
   );
 }
