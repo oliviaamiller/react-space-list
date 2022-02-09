2265117a4c81ff5251808daa9eb440303c4db9f6
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-08 17:33:34 -0800

    added page buttons

--- src/OrbPage.js
+++ src/OrbPage.js
@@ -1,7 +1,36 @@
-
+import { useEffect, useState } from 'react';
+import { getOrbs } from './services/fetch-utils';
+import OrbList from './OrbList';
 
 export default function OrbPage() {
+  const [orbs, setOrbs] = useState([]);
+  const [page, setPage] = useState(1);
+  const onPage = 40;
+
+  useEffect(() => {
+    async function fetch() {
+      const from = page * onPage - onPage;
+      const to = page * onPage;
+      const space = await getOrbs(from, to);
+
+      setOrbs(space);
+    }
+    fetch();
+  }, [page]);
+
+
   return (
-    <div>OrbPage</div>
+    <div>
+      <p>page {page}</p>
+      <div className='page-buttons'>
+        <button
+          onClick={() => setPage(page - 1)}
+          disabled={page === 1}>Previous Page</button>
+
+        <button
+          onClick={() => setPage(page + 1)}
+          disabled={orbs.length < onPage}>Next Page</button>
+      </div>
+    </div>
   );
 }
